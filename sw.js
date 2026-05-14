0808c060e4c82c9d6c632d38aad086bd3cc36355
rppeccinin-maker <rppeccinin@gmail.com>
2026-05-13 21:55:27 -0300

    Implement service worker caching and fetch handling
    Add service worker event listeners for install and fetch.

--- sw.js
+++ sw.js
@@ -1,1 +1,6 @@
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('rpp-v2').then(cache => cache.addAll(['./', './index.html'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+});
