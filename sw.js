b5b4e9ca5ce99bb0c0db151ea4db7b471a72a0cc
rppeccinin-maker <rppeccinin@gmail.com>
2026-05-14 18:00:52 -0300

    Update service worker caching strategy

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
+const cacheName = 'rpp-v1';
+const assets = ['./', './index.html', './manifest.json', './icon.png'];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open('rpp-v2').then(cache => cache.addAll(['./', './index.html'])));
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
